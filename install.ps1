c7e08bc4275782687bfe3d75ce01185ccb15eda8
ToG-keyz <mailacctai@gmail.com>
2026-07-20 22:30:48 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,27 +1,41 @@
 $InstallDir = "$env:LOCALAPPDATA\ToG"
 
+Write-Host ""
+Write-Host "==================================="
+Write-Host "      Installing ToG..."
+Write-Host "==================================="
+Write-Host ""
+
+# Tạo thư mục
 New-Item -ItemType Directory -Force -Path $InstallDir | Out-Null
 
+# Tải opentog.bat
 Invoke-WebRequest `
     -Uri "https://raw.githubusercontent.com/tog-keyz/tog-system/main/opentog.bat" `
     -OutFile "$InstallDir\opentog.bat"
 
-$userPath = [Environment]::GetEnvironmentVariable("Path","User")
+# Tải update.ps1
+Invoke-WebRequest `
+    -Uri "https://raw.githubusercontent.com/tog-keyz/tog-system/main/update.ps1" `
+    -OutFile "$InstallDir\update.ps1"
 
-if ($userPath -notlike "*$InstallDir*") {
+# Thêm PATH nếu chưa có
+$UserPath = [Environment]::GetEnvironmentVariable("Path","User")
+
+if ($UserPath -notlike "*$InstallDir*") {
     [Environment]::SetEnvironmentVariable(
         "Path",
-        "$userPath;$InstallDir",
+        "$UserPath;$InstallDir",
         "User"
     )
 }
 
 Write-Host ""
 Write-Host "==================================="
-Write-Host "ToG installed successfully!"
+Write-Host "  ToG installed successfully!"
 Write-Host "==================================="
 Write-Host ""
-Write-Host "Please reopen CMD then type:"
+Write-Host "Restart CMD then type:"
 Write-Host ""
 Write-Host "    opentog"
 Write-Host ""
