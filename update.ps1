45bccacf8f7bdb7b0968f612ad7865326e9372ab
ToG-keyz <mailacctai@gmail.com>
2026-07-20 22:31:03 +0700

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -1,16 +1,24 @@
 $InstallDir = "$env:LOCALAPPDATA\ToG"
 
 Write-Host ""
-Write-Host "Updating ToG..."
+Write-Host "==================================="
+Write-Host "      Updating ToG..."
+Write-Host "==================================="
 Write-Host ""
 
+# Tải opentog.bat
 Invoke-WebRequest `
     -Uri "https://raw.githubusercontent.com/tog-keyz/tog-system/main/opentog.bat" `
     -OutFile "$InstallDir\opentog.bat"
 
+# Tự cập nhật update.ps1
+Invoke-WebRequest `
+    -Uri "https://raw.githubusercontent.com/tog-keyz/tog-system/main/update.ps1" `
+    -OutFile "$InstallDir\update.ps1"
+
 Write-Host ""
 Write-Host "==================================="
-Write-Host " ToG updated successfully!"
+Write-Host "  ToG updated successfully!"
 Write-Host "==================================="
 Write-Host ""
-Write-Host "Restart opentog to use the new version."
+Write-Host "Restart opentog."
